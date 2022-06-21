a5d0639745d556d19ecea35d3e43dbcca78e6fc4
sehdev <sehdev@atai.ai>
2022-06-21 17:49:27 +0530

    new commit

--- karma.conf.js
+++ karma.conf.js
@@ -3,30 +3,44 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+      // ! start sonar reports configuration
+      require("karma-phantomjs-launcher"),
+      require("karma-sonarqube-unit-reporter"),
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    // ! start sonar reports configuration
+    sonarQubeUnitReporter: {
+      sonarQubeVersion: "LATEST",
+      outputFile: "reports/ut_report.xml",
+      useBrowserName: false,
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/integration1'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      dir: require("path").join(__dirname, "./coverage/integration1"),
+      reports: ["html", "lcovonly", "text-summary"],
+      fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml", "sonarqubeUnit"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    // ! start sonar reports configuration
+    browsers: ["PhantomJS", "ChromeHeadless", "Chrome"],
+    customLaunchers: {
+      ChromeHeadlessCI: { base: "ChromeHeadless", flags: ["--no-sandbox"] },
+    },
   });
 };
